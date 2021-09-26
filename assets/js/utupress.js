2142d3ad6cd67462f121059b4dc11154f226ef23
Dedan Irungu <dedanirungu@gmail.com>
2021-09-26 14:35:28 +0300

    changes

--- assets/js/utupress.js
+++ assets/js/utupress.js
@@ -5,33 +5,8 @@
    var db = new Dexie("utupress");
    db.version(1).stores({
        settings: 'name,value',
-       cache: 'name,value'
+       cached: 'name,value'
    });
-
-   //
-   // Put some data into it
-   //
-   db.settings.put({
-       name: "limit",
-       value: 20
-   }).then(function () {
-       //
-       // Then when data is stored, read from it
-       //
-       return db.settings.get('limit');
-   }).then(function (friend) {
-       //
-       // Display the result
-       //
-   }).catch(function (error) {
-       //
-       // Finally don't forget to catch any error
-       // that could have happened anywhere in the
-       // code blocks above.
-       //
-       alert("Ooops: " + error);
-   });
-
 
 
    /**xxxxxxxxxxxxxxxxxxxxxxxxxxxxx  Store xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx */
